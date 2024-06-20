bbf9506590d500b0e393d05ab3e4d92152f0faad
anjanayraina <anjanayraina326@gmail.com>
2024-06-20 09:37:15 +0530

    variables and constants added

--- W3School/HelloWorld.go
+++ W3School/HelloWorld.go
@@ -1,8 +1,9 @@
-package main   
-import ("fmt")
+package main
 
-func main() {  
-  
-fmt.Println("Hello World!");
+import "fmt"
+
+func main() {
+
+	fmt.Println("Hello World!")
 
 }
--- W3School/variables.go
+++ W3School/variables.go
@@ -1,16 +1,22 @@
-package main;
+package main
 
-import("fmt")
+import (
+	"fmt"
+)
 
 func main() {
-	//Single Variable Decleration 
-	 
-	var item1 int  =10; // standard declearation of a variable 
-	item2 := "Anjanay Raina"; // inferred declaration of the type of the variable 
-	fmt.Printf("Item1 type : %T and Item2 type : %T \n" , item1 , item2);
+	//Single Variable Decleration
 
-	// Multiple Variable Decleration 
+	var item1 int = 10       // standard declearation of a variable
+	item2 := "Anjanay Raina" // inferred declaration of the type of the variable
+	fmt.Printf("Item1 type : %T and Item2 type : %T \n", item1, item2)
 
-	var a , b , c = 10 , "Hello" , 12.34; 
-	fmt.Println(a , b , c);
+	// Multiple Variable Decleration
+
+	var a, b, c = 10, "Hello", 12.34
+	fmt.Println(a, b, c)
+
+	// constants
+	const PI = 3.14
+	fmt.Println(PI)
 }
