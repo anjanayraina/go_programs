a67e7ec08d3e880d20c337c63280bd6db637ad4f
anjanayraina <anjanayraina326@gmail.com>
2024-06-22 08:37:58 +0530

    Iteration Added

---
+++ Testing/Iteration/repeat_test.go
@@ -0,0 +1,11 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("A", 5)
+	expected := "AAAAA"
+	if repeated != expected {
+		t.Errorf("The expected value is %q but we got %q", expected, repeated)
+	}
+}
